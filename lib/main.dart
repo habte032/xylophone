35c586d77103193cf049d6792862019654cf4f17
habte032 <habte032@gmail.com>
2023-10-15 00:21:42 +0300

    add note1

--- lib/main.dart
+++ lib/main.dart
@@ -36,79 +36,7 @@ class MyApp extends StatelessWidget {
                     child: const Text('1')
                 ),
               ),
-              Expanded(
-                child: TextButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white10)),
 
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(
-                      AssetSource('note2.wav'),
-                    );
-                  },
-                    child: const Text('2')
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blueGrey.shade900)),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(
-                      AssetSource('note3.wav'),
-                    );
-                  },
-                    child: const Text('3')
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white54)),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(
-                      AssetSource('note4.wav'),
-                    );
-                  },
-                    child: const Text('4')
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blueGrey.shade900)),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(
-                      AssetSource('note5.wav'),
-                    );
-                  },
-                    child: const Text('5')
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white30)),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(
-                      AssetSource('note6.wav'),
-                    );
-                  },
-                  child: const Text('6')
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blueGrey.shade900)),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    await player.play(
-                      AssetSource('note7.wav'),
-                    );
-                  },
-                  child: const Text('7')
-                ),
-              ),
 
             ],
           ),
