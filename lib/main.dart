d0fd864fa348b6cc6b7201ce047586d6cda0eec5
habte032 <habte032@gmail.com>
2024-10-21 23:43:20 +0300

    optimize

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-// import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(const MyApp());
